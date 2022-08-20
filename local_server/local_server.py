baa346b3776bc08a48e4758dcb12119406b1b473
André Déo <andredeo@gmail.com>
2022-08-20 19:25:29 -0300

    Http_sd_configs Improvements

---
+++ local_server/local_server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from urllib import response
+import json
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', defaults={"err": ""})
+@app.errorhandler(404)
+@app.errorhandler(405)
+def print_request(err):
+    return "OK\n"
+@app.route("/sd")
+def print_sd():
+    data={"targets":["prometheus.io"],"labels":{"__meta_datacenter":"london","__meta_prometheus_job":"node"}}
+    response=app.response_class("[{}]".format(data).replace('\'','"').replace(' ',''),mimetype='application/json')
+    return(response)
+if __name__ == '__main__':
+    app.run("0.0.0.0", port=8003)
