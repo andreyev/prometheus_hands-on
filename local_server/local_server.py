e0f5c6c5a3318f72e058a626ef539b0a959011c5
André Déo <andredeo@gmail.com>
2022-08-21 11:55:01 -0300

    simplifying the code

--- local_server/local_server.py
+++ local_server/local_server.py
@@ -12,8 +12,7 @@ def print_request(err):
     return "OK\n"
 @app.route("/sd")
 def print_sd():
-    data={"targets":["prometheus.io"],"labels":{"__meta_datacenter":"london","__meta_prometheus_job":"node"}}
-    response=app.response_class("[{}]".format(data).replace('\'','"').replace(' ',''),mimetype='application/json')
-    return(response)
+    data=app.response_class('[{"targets":["prometheus.io"],"labels":{"__meta_datacenter":"london","__meta_prometheus_job":"node"}}]',mimetype='application/json')
+    return(data)
 if __name__ == '__main__':
     app.run("0.0.0.0", port=8003)
